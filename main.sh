3e543ecdd696f2b5499de9da92439215a5c5b042
Pedro Gomes <pedrogbranquinho@gmail.com>
2024-06-28 12:11:12 -0300

    doc, refactor: update docs, separate xmonad setup and install steps

--- main.sh
+++ main.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/bash
-#UBUNTU=${uname-a | grep "Ubuntu"}
+export DOTFILES=$(pwd)
+DOTFILES=$(pwd)
 
-#ln -sf $DOTFILES/.bashrc $HOME/.bashrc
-# bash ./setup/link-config
-#ln -sf $DOTFILES/.local $HOME/.local
-#ln -sf $DOTFILES/.config $HOME/.config
-
+ln -sf $DOTFILES/.bashrc $HOME/.bashrc
 rm -rf $DOTFILES/gitthings
-
 SC="$DOTFILES/scripts/"
 cd $SC
 bash ./setup/bk-dots
 bash ./setup/init
+bash ./scripts/setup/link-config
 
 source $HOME/.bashrc
 
 bash ./install/main
+# make newly installed fonts available
 fc-cache -vf
+
+# Install the window manager
 bash ./setup/xmonad
+bash ./install/xmonad
 
 echo "Congrats. If everything went well, you have the newest Buddhi WM installed."
+echo "New step, you can logout from your current Ubuntu session, and chose XMonad,"
+echo "instead of GNOME Window Manager. This choice is generally done at the login"
+echo "interface."
