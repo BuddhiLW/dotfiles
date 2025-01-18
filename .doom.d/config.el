a30480aee11207734852b766c74e86bcc15c39e7
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-01-18 01:53:50 -0300

    update dots

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1330,5 +1330,25 @@
         :envFile nil
         :dlvToolPath "dlv")) ;; Ensure `dlv` is in PATH
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+(require 'eaf)
+
+(require 'eaf-browser)
+;; (require 'eaf-pdf-viewer)
+
+(use-package! eaf
+  :load-path "~/.emacs.d/site-lisp/emacs-application-framework"
+  :custom
+  ; See https://github.com/emacs-eaf/emacs-application-framework/wiki/Customization
+  (eaf-browser-continue-where-left-off t)
+  (eaf-browser-enable-adblocker t)
+  (browse-url-browser-function 'eaf-open-browser)
+  :config
+  (defalias 'browse-web #'eaf-open-browser)
+  ;; (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  ;; (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  ;; (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
+
 (server-force-delete)
 (server-start)
